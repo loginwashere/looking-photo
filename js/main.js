524d15bee09082a7a031e77e7d84da26d8f6aa0a
Dmitriy Savchenko <login.was.here@gmail.com>
2012-01-09 22:46:45 +0200

    Keep rewriting

--- js/main.js
+++ js/main.js
@@ -6,153 +6,163 @@
 /*
  * This element contain foto image
  */
-var photo = {
-     element: null,
-     border: {
-         left: null,
-         right: null,
-         top: null,
-         bottom: null
-     },
-    setBackground: function(value) {
-        this.element.css('background-position', value);
+var Item = (function() {
+    return {
+        element: null,
+        border: {
+            left: null,
+            right: null,
+            top: null,
+            bottom: null
+        },
+        findBorders: function() {
+            this.border = this.element.offset();
+            this.border.right = this.border.left + this.element.width();
+            this.border.bottom = this.border.top + this.element.height();
+        }
+    };
+})();
+var photo = Object.create(Item, {
+    change: {
+        value: function(mouse) {
+            switch (true) {
+                case this.condition.topLeft(mouse, this.border):
+                    this.setBackground(this.positions.topLeft);
+                    break;
+                case this.condition.topRight(mouse, this.border):
+                    this.setBackground(this.positions.topRight);
+                    break;
+                case this.condition.top(mouse, this.border):
+                    this.setBackground(this.positions.topCenter);
+                    break;
+                case this.condition.centerLeft(mouse, this.border):
+                    this.setBackground(this.positions.centerLeft);
+                    break;
+                case this.condition.center(mouse, this.border):
+                    this.setBackground(this.positions.centerCenter);
+                    break;
+                case this.condition.centerRight(mouse, this.border):
+                    this.setBackground(this.positions.centerRight);
+                    break;
+                case this.condition.bottomLeft(mouse, this.border):
+                    this.setBackground(this.positions.bottomLeft);
+                    break;
+                case this.condition.bottom(mouse, this.border):
+                    this.setBackground(this.positions.bottomCenter);
+                    break;
+                case this.condition.bottomRight(mouse, this.border):
+                    this.setBackground(this.positions.bottomRight);
+                    break;
+            }
+        }
+    },
+    setBackground: {
+        value: function(value) {
+            this.element.css('backgroundPosition', value);
+        }
+    },
+    /*
+     *  Object represents coordinates of upper left corner for parts of image
+     *  Image consists of nine fotos, joined horizontaly
+     *  Latter these comments will be inserted in background-position css property
+     *
+     */
+    positions: {
+        value: (function(){
+            var step = -200;
+            return {
+                topLeft: 1*step + 'px 0', // coordinates of part of foto with same name
+                topCenter: 2*step + 'px 0',
+                topRight: 3*step + 'px 0',
+                centerLeft: 4*step + 'px 0',
+                centerCenter: 0*step + ' 0',
+                centerRight: 6*step + 'px 0',
+                bottomLeft: 7*step + 'px 0',
+                bottomCenter: 8*step + 'px 0',
+                bottomRight: 9*step + 'px 0',
+                initial: 5*step + 'px 0',
+                fun: 10*step + 'px 0'
+            };
+        })()
+    },
+    condition: {
+        value: {
+            topLeft: function(mouse, border) {
+                return mouse.x < border.left
+                    && mouse.y < border.top;
+            },
+            top: function(mouse, border) {
+                return mouse.x < border.right
+                    && mouse.x > border.left
+                    && mouse.y < border.top;
+            },
+            topRight: function(mouse, border) {
+                return mouse.x > border.right
+                    && mouse.y < border.top;
+            },
+            centerLeft: function(mouse, border) {
+                return mouse.x < border.left
+                    && mouse.y < border.bottom
+                    && mouse.y > border.top;
+            },
+            center: function(mouse, border) {
+                return mouse.x < border.right
+                    && mouse.x > border.left
+                    && mouse.y < border.bottom
+                    && mouse.y > border.top;
+            },
+            centerRight: function(mouse, border) {
+                return mouse.x > border.right
+                    && mouse.y < border.bottom
+                    && mouse.y > border.top;
+            },
+            bottomLeft: function(mouse, border) {
+                return mouse.x < border.left
+                    && mouse.y > border.bottom;
+            },
+            bottom: function(mouse, border) {
+                return mouse.x < border.right
+                    && mouse.x > border.left
+                    && mouse.y > border.bottom;
+            },
+            bottomRight: function(mouse, border) {
+                return mouse.x > border.right
+                    && mouse.y > border.bottom;
+            }
+        }
     }
- };
+});
 /*
  * This element contain element, that contain foto image
  */
-var container = {
-    element: null,
-    border: {
-        left: null,
-        right: null,
-        top: null,
-        bottom: null
+var container = Object.create(Item, {
+    condition: {
+        value: {
+            insideContainer: function(mouse, border) {
+                return mouse.x > border.left
+                    && mouse.x < border.right
+                    && mouse.y > border.top
+                    && mouse.y < border.bottom;
+            }
+        }
     }
-};
-/*
- *  Object represents coordinates of upper left corner for parts of image
- *  Image consists of nine fotos, joined horizontaly
- *  Latter these comments will be inserted in background-position css property
- *
- */
-var step = -200;
-var positions = {
-    topLeft: 1*step + 'px 0', // coordinates of part of foto with same name
-    topCenter: 2*step + 'px 0',
-    topRight: 3*step + 'px 0',
-    centerLeft: 4*step + 'px 0',
-    centerCenter: 0*step + ' 0',
-    centerRight: 6*step + 'px 0',
-    bottomLeft: 7*step + 'px 0',
-    bottomCenter: 8*step + 'px 0',
-    bottomRight: 9*step + 'px 0',
-    initial: 5*step + 'px 0',
-    fun: 10*step + 'px 0'
-};
-var condition = {
-    insideContainer: function(x, y, border) {
-        return x > border.left
-            && x < border.right
-            && y > border.top
-            && y < border.bottom;
-    },
-    topLeft: function(x, y, border) {
-        return x < border.left
-            && y < border.top;
-    },
-    top: function(x, y, border) {
-        return x < border.right
-            && x > border.left
-            && y < border.top;
-    },
-    topRight: function(x, y, border) {
-        return x > border.right
-            && y < border.top;
-    },
-    centerLeft: function(x, y, border) {
-        return x < border.left
-            && y < border.bottom
-            && y > border.top;
-    },
-    center: function(x, y, border) {
-        return x < border.right
-            && x > border.left
-            && y < border.bottom
-            && y > border.top;
-    },
-    centerRight: function(x, y, border) {
-        return x > border.right
-            && y < border.bottom
-            && y > border.top;
-    },
-    bottomLeft: function(x, y, border) {
-        return x < border.left
-            && y > border.bottom;
-    },
-    bottom: function(x, y, border) {
-        return x < border.right
-            && x > border.left
-            && y > border.bottom;
-    },
-    bottomRight: function(x, y, border) {
-        return x > border.right
-            && y > border.bottom;
-    }
-};
-/*
- * Calculate coordinates of bordes of given element
- */
-function findBorders(element) {
-    var position = element.element.offset();
-    element.border.left = position.left;
-    element.border.right = element.border.left + element.element.width();
-    element.border.top = position.top;
-    element.border.bottom = element.border.top + element.element.height();
-}
-function changePhoto(x, y, photo) {
-    switch (true) {
-        case condition.topLeft(x, y, photo.border):
-            photo.setBackground(positions.topLeft);
-            break;
-        case condition.topRight(x, y, photo.border):
-            photo.setBackground(positions.topRight);
-            break;
-        case condition.top(x, y, photo.border):
-            photo.setBackground(positions.topCenter);
-            break;
-        case condition.centerLeft(x, y, photo.border):
-            photo.setBackground(positions.centerLeft);
-            break;
-        case condition.center(x, y, photo.border):
-            photo.setBackground(positions.centerCenter);
-            break;
-        case condition.centerRight(x, y, photo.border):
-            photo.setBackground(positions.centerRight);
-            break;
-        case condition.bottomLeft(x, y, photo.border):
-            photo.setBackground(positions.bottomLeft);
-            break;
-        case condition.bottom(x, y, photo.border):
-            photo.setBackground(positions.bottomCenter);
-            break;
-        case condition.bottomRight(x, y, photo.border):
-            photo.setBackground(positions.bottomRight);
-            break;
-    }
-}
+});
+
 $(document).ready(function() {
 	container.element = $(".container");
     photo.element = $("#myfoto");
 	/*
 	 * Set initial image
 	 */
-    photo.setBackground(positions.initial);
+    photo.setBackground(photo.positions.initial);
 	/*
 	 * Change image, when it was clicked
 	 */
-    photo.element.click(function(event) {
-        photo.setBackground(positions.fun);
+    photo.element.bind('click', function(event) {
+        if (!photo.element.hasClass('fun')) {
+            photo.element.addClass('fun');
+        }
+        photo.setBackground(photo.positions.fun);
         event.preventDefault();
 	});
 	/*
@@ -160,17 +170,23 @@ $(document).ready(function() {
 	 */
 	container.element.mousemove(function(event) {
 		// Calculate coordinates of borders of foto and container elements
-		findBorders(photo);
-		findBorders(container);
-        var x = event.pageX,
-            y = event.pageY;
-		// Check if mouse is inside container
-		if (condition.insideContainer(x, y, container.border)) {
-            changePhoto(x, y, photo);
-		}
+        if (!photo.element.hasClass('fun')) {
+            photo.findBorders();
+            container.findBorders();
+            var mouse = {
+                x: event.pageX,
+                y: event.pageY
+            };
+            // Check if mouse is inside container
+            if (container.condition.insideContainer(mouse, container.border)) {
+                photo.change(mouse);
+            }
+        } else {
+            photo.element.removeClass('fun');
+        }
 	});
-    container.element.mouseleave(function(event){
+    container.element.mouseleave(function(){
         // If mouse is not inside container then show initial image
-        photo.setBackground(positions.initial);
+        photo.setBackground(photo.positions.initial);
     });
 });
